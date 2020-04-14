eba4c208b5cca9d873b203efa809e018c988033a
Binh  🌱 <binhrobles@gmail.com>
2020-04-13 19:31:23 -0700

    Update k8s images with SHA references

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,23 @@
 # Docker image building and pushing
-docker build -t binhrobles/complex-client -f ./client/Dockerfile ./client
-docker build -t binhrobles/complex-server -f ./server/Dockerfile ./server
-docker build -t binhrobles/complex-worker -f ./worker/Dockerfile ./worker
-docker push binhrobles/complex-client
-docker push binhrobles/complex-server
-docker push binhrobles/complex-worker
+docker build -t binhrobles/complex-client:latest -t binhrobles/complex-client:$SHA \
+  -f ./client/Dockerfile ./client
+docker build -t binhrobles/complex-server:latest -t binhrobles/complex-server:$SHA \
+  -f ./server/Dockerfile ./server
+docker build -t binhrobles/complex-worker:latest -t binhrobles/complex-worker:$SHA \
+  -f ./worker/Dockerfile ./worker
+docker push binhrobles/complex-client:latest
+docker push binhrobles/complex-server:latest
+docker push binhrobles/complex-worker:latest
+docker push binhrobles/complex-client:$SHA
+docker push binhrobles/complex-server:$SHA
+docker push binhrobles/complex-worker:$SHA
 
 # Kubernetes configuration applying
 kubectl apply -f k8s
 
-kubectl rollout restart deployment/client-deployment
+kubectl set image deployments/client-deployment client=binhrobles/complex-client:$SHA
+kubectl set image deployments/server-deployment server=binhrobles/complex-server:$SHA
+kubectl set image deployments/worker-deployment worker=binhrobles/complex-worker:$SHA
+
+# could also just use this?
+# kubectl rollout restart deployment/client-deployment
